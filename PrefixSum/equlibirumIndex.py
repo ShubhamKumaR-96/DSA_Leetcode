724bfe3a88f8cb8bd0a6a03c73b1a3de2285043b
ShubhamKumaR-96 <shubhamkas245@gmail.com>
2025-05-07 09:57:33 +0530

    equlibrium index ques

---
+++ PrefixSum/equlibirumIndex.py
@@ -0,0 +1,14 @@
+def findEquilibriumIndex(nums):
+    total_sum = sum(nums)  
+    left_sum = 0  
+    
+    for i in range(len(nums)):  
+        if left_sum == total_sum - left_sum - nums[i]:  # Equilibrium condition
+            return i  # Return index
+        left_sum += nums[i]  
+    
+    return -1  # If no equilibrium index found
+
+# Example usage
+nums = [-7, 1, 5, 2, -4, 3, 0]
+print(f"Equilibrium Index: {findEquilibriumIndex(nums)}")
